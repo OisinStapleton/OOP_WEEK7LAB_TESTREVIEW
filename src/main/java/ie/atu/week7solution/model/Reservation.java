b78de0b89175fd0a6819538ee1b499c9d4231792
OisinStapleton <G00365445@atu.ie>
2026-03-11 10:49:34 +0000

    Completion of reservation class.

--- src/main/java/ie/atu/week7solution/model/Reservation.java
+++ src/main/java/ie/atu/week7solution/model/Reservation.java
@@ -1,4 +1,33 @@
 package ie.atu.week7solution.model;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Reservation {
+    private Long reservationID;
+
+    @NotBlank(message = "EquipmentTag is required")
+    private String equipmentTag;
+
+    @NotBlank(message = "studentEmail is required")
+    @Email(message = "studentEmail must be a valid email address")
+    private String studentEmail;
+
+    @NotNull(message = "ReservationDate is required")
+    private LocalDate reservationDate;
+
+    @Min(value = 0, message = "Start hour must be between 0 & 23")
+    @Max(value = 23, message = "Start hour must be between 0 & 23")
+    private int startHour;
+
 }
