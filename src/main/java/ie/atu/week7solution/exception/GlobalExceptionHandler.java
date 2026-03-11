fb802f8aa79abe5cfe3dd4140721c2a48cf356ba
OisinStapleton <G00365445@atu.ie>
2026-03-11 11:31:48 +0000

    Exception handler created.

--- src/main/java/ie/atu/week7solution/exception/GlobalExceptionHandler.java
+++ src/main/java/ie/atu/week7solution/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package ie.atu.week7solution.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>>  handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new  ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
