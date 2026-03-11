0de720db06721903c75f7b678b7162fd3f3cfc51
OisinStapleton <G00365445@atu.ie>
2026-03-11 11:09:13 +0000

    Reservation controller done & work on service class.

--- src/main/java/ie/atu/week7solution/controller/ReservationController.java
+++ src/main/java/ie/atu/week7solution/controller/ReservationController.java
@@ -1,4 +1,28 @@
 package ie.atu.week7solution.controller;
 
+import ie.atu.week7solution.model.Reservation;
+import ie.atu.week7solution.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
+
+    private final ReservationService reservationService; // tells springboot that it is responsible for class creation
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    // create
+    @PostMapping
+    public ResponseEntity<Reservation> create(@Valid @RequestBody Reservation reservation) {
+        Reservation saved = reservationService.addReservation(reservation);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
 }
--- src/main/java/ie/atu/week7solution/service/ReservationService.java
+++ src/main/java/ie/atu/week7solution/service/ReservationService.java
@@ -1,4 +1,14 @@
 package ie.atu.week7solution.service;
 
+import ie.atu.week7solution.model.Reservation;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
 public class ReservationService {
+
+    public Reservation addReservation(@Valid @RequestBody Reservation reservation) {
+        return reservation;
+    }
 }
